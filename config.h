07abd5a51a62ae8cff3d9eb5c70a5507ba92e8ed
boroveen <56752476+boroveen@users.noreply.github.com>
2022-02-05 11:12:23 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-#define DAYCOOKIE "give me cookie:("
-#define COOKIE    "thank you:)"
-#define STATUS    "cookies: "
+#define DAYCOOKIE "give me cookie:(" //cookie
+#define COOKIE    "thank you:)" //cookie cookie
+#define STATUS    "cookies: " //cookie status
 #define TIME      120 //second
